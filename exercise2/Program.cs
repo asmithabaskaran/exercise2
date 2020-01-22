8d6cb514f2c96e6a4c3a3091777d664303035282
Asmitha Baskaran <baskaran@mail.usf.edu>
2020-01-21 21:03:27 -0500

    changes 1

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -10,6 +10,17 @@ namespace exercise2
     {
         static void Main(string[] args)
         {
+            int age;
+            int height;
+            //Max heart rate is equal to 220 - age
+            int heart;
+            age = 26;
+            height = 170;
+            heart = 220 - age;
+
+            //Write I am ## years old, my max heart rate is ## and I am ## cms tall
+            Console.WriteLine("I am " + age  + " years old and my max heart rate is " + heart  + " and I am " + height  + " cms tall");
+            Console.ReadLine();
         }
     }
 }
